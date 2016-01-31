94e49b1bafc3f2db9878c21ead215c09dd9e89be
Toby Lawrence <toby@nuclearfurnace.com>
2016-01-31 09:06:36 -0500

    Soup up our search capabilities.
    Now we assigned a match factor based on what piece of information was matched: name matches are treated as higher than a flavor text match, etc.  We were implicitly ordering things by set release order, because of the ordering in the JSON data and how we were searching it.

--- scryer.js
+++ scryer.js
@@ -1,28 +1,55 @@
 var fs = require('fs-promise')
+var winston = require('winston')
+var moment = require('moment')
+var firstBy = require('thenby')
 var express = require('express')
 var app = express()
+
+// Set up our logging.
+winston.loggers.add('scryer', {
+  console: {
+    colorize: true,
+    timestamp: function() {
+      return moment().format()
+    }
+  }
+});
+
+var logging = winston.loggers.get('scryer')
 
 // Read in our data.
 var cardData = fs.readJsonSync('./card-data.json')
 
-function doesCardMatchQuery(card, query) {
-  if(hasMatchingAttribute(card, 'name', query)) {
-    return true
-  }
+function attemptCardMatch(card, query) {
+  var matches = false
 
-  if(hasMatchingAttribute(card, 'text', query)) {
-    return true
+  if(hasMatchingAttribute(card, 'artist', query)) {
+    matches = true
+    card['match_factor'] = 600
   }
 
   if(hasMatchingAttribute(card, 'flavor', query)) {
-    return true
+    matches = true
+    card['match_factor'] = 700
   }
 
-  if(hasMatchingAttribute(card, 'artist', query)) {
-    return true
+  if(hasMatchingAttribute(card, 'text', query)) {
+    matches = true
+    card['match_factor'] = 800
   }
 
-  return false
+  if(hasMatchingAttribute(card, 'name', query)) {
+    matches = true
+
+    // If we have a name match, see if it's the full name.
+    if(card['name'].toLowerCase() == query.toLowerCase()) {
+      card['match_factor'] = 1000
+    } else {
+      card['match_factor'] = 900
+    }
+  }
+
+  return {card: card, matches: matches}
 }
 
 function hasMatchingAttribute(card, attr, value) {
@@ -33,8 +60,11 @@ function hasMatchingAttribute(card, attr, value) {
   return card[attr].toString().toLowerCase().indexOf(value) >= 0
 }
 
+function sortedCards(cards) {
+  return cards.sort(firstBy('match_factor', -1).thenBy('name'))
+}
+
 function searchCards(cardData, query) {
-  console.log("querying cards for '" + query + "'")
   var matchingCards = {}
 
   var setCodes = Object.keys(cardData)
@@ -45,17 +75,16 @@ function searchCards(cardData, query) {
       var currentCard = setData['cards'][j]
       currentCard['set'] = setCodes[i]
 
-      if(doesCardMatchQuery(currentCard, query)) {
-        console.log('found match: ' + currentCard['name'] + ' (' + currentCard['multiverseid'] + ')')
-
-        var existingCard = matchingCards[currentCard['name']]
+      var matchData = attemptCardMatch(currentCard, query)
+      if(matchData['matches']) {
+        var matchedCard = matchData['card']
+        var existingCard = matchingCards[matchedCard['name']]
         if(existingCard == undefined) {
-          existingCard = currentCard
+          existingCard = matchedCard
         }
 
-        if(currentCard['multiverseid'] >= existingCard['multiverseid']) {
-          console.log('card is newer than previous match (' + currentCard['multiverseid'] + ' vs ' + existingCard['multiverseid'] + ')')
-          matchingCards[currentCard['name']] = currentCard
+        if(matchedCard['multiverseid'] >= existingCard['multiverseid']) {
+          matchingCards[matchedCard['name']] = matchedCard
         }
       }
     }
@@ -68,19 +97,23 @@ function searchCards(cardData, query) {
     flattenedMatchingCards.push(actualCard)
   }
 
-  return flattenedMatchingCards
+  return sortedCards(flattenedMatchingCards)
 }
 
 // And go.
 app.use(express.static('app/public'))
 
 app.get('/search/:query', function(request, response) {
-  console.log('query: ' + request.params.query)
+  logging.log('info', 'Query request: %s', request.params.query)
 
+  var startTime = Date.now()
   var matchingCards = searchCards(cardData, request.params.query)
+  var delta = Date.now() - startTime
+
+  logging.log('info', 'Search took %dms to execute, returning %s matches.', delta, matchingCards.length)
+
   response.json({count: matchingCards.length, cards: matchingCards})
 })
 
-app.listen(3000, function() {
-  console.log('now listening on port 3000!')
-})
+logging.info('Listening for requests on port 3000!')
+app.listen(3000)
